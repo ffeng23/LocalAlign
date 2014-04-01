c7ba9caca32a2523375880637915a8f63cd00927
Feng Feng <feng@Iris.bumc.bu.edu>
2014-04-01 14:58:20 -0400

    add new files, testingRcode.r for running equivalent code for testing purpose; two score matrices for testing by R code. 4/1/2014 feng

---
+++ testingRcode.r
@@ -0,0 +1,33 @@
+
+ #load
+ library('Biostrings')
+
+
+NUC44 <- as.matrix(read.table("NUC.4.4", check.names=FALSE))
+BLOSUM50<-as.matrix(read.table("BLOSUM50", check.names=FALSE))
+tm<-as.matrix(read.table("TestMatrix", check.names=FALSE))
+tm2<-as.matrix(read.table("TestMatrix2", check.names=FALSE))
+#define gap
+gapOpening<- -0;
+gapExtension<- -20;
+
+seq1<-AAString("VSPAGMASGYDPGKA");
+seq2 <-AAString("IPGKATREYDVSPAG");
+
+LocalAlign <-    pairwiseAlignment(seq1, seq2, type = "global", substitutionMatrix = BLOSUM50, gapOpening = gapOpening, gapExtension = gapExtension)
+
+
+seq1<-AAString("ATAT");
+seq2 <-AAString("ACHKAT");
+
+seq1<-AAString("ATCGA");
+seq2 <-AAString("GATTGA");
+seq1<-AAString("AGCTAGAGACCAGTCTGAGGTAGA");
+seq2 <-AAString("AGCTAGAGACCAGCTATCTAGAGGTAGA");
+
+seq1<-DNAString("CCAATCTACTACTGCTTGCAGTAC");
+
+seq2<-DNAString("AGTCCGAGGGCTACTCTACTGAAC");
+
+
+LocalAlign <-    pairwiseAlignment(seq1, seq2, type = "local", substitutionMatrix = tm2, gapOpening = gapOpening, gapExtension = gapExtension)
